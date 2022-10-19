4afec4d01862b36f90f35eaba9ed3ab8cfad51dd
Oleksandr Matsko <Oleksandr.Matsko@sitegeist.de>
2022-10-19 12:51:09 +0200

    Remove all 'Use' statements from 'ext_tables.php'

--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,12 @@
 <?php
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(
     function () {
 
-        ExtensionManagementUtility::addStaticFile('translatelabels', 'Configuration/TypoScript', 'translate_labels');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('translatelabels', 'Configuration/TypoScript', 'translate_labels');
 
-        ExtensionManagementUtility::addLLrefForTCAdescr('tx_translatelabels_domain_model_translation', 'EXT:translatelabels/Resources/Private/Language/locallang_csh_tx_translatelabels_domain_model_translation.xlf');
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_translatelabels_domain_model_translation');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_translatelabels_domain_model_translation', 'EXT:translatelabels/Resources/Private/Language/locallang_csh_tx_translatelabels_domain_model_translation.xlf');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_translatelabels_domain_model_translation');
     }
 );
